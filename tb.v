e7c345845f39e4603eb59f2eb58467f2d9de4e6c
Avronil <basuavronil65@gmail.com>
2026-08-28 00:16:32 +0530

    Refactor testbench for sync_fifo module

--- tb.v
+++ tb.v
@@ -1,77 +1,28 @@
-`timescale 1ns/1ps
-
-module tb_fifo;
-
-    reg clk, rst_n, wr_en, rd_en;
-    reg  [7:0]  din;
-    wire [7:0]  dout;
-    wire        full, empty;
-
-    fifo dut (.clk(clk), .rst_n(rst_n),
-        .wr_en(wr_en),
-        .rd_en(rd_en),
-        .din(din),
-        .dout(dout),
-        .full(full),
-        .empty(empty)
-    );
-
-    initial begin
-     clk = 0;
-     forever #5 clk = ~ clk;
+module tb;
+  reg clk, rst, wr_en, rd_en;
+  reg [7:0] data_in;
+  wire empty, full;
+  wire [7:0] data_out;
+  sync_fifo dut (.clk(clk), .rst(rst), .wr_en(wr_en), .rd_en(rd_en), .data_in(data_in), .data_out(data_out), .empty(empty), .full(full));
+  initial begin
+    clk = 1'd0;
+    forever #5 clk = ~clk;
+  end
+  initial begin
+    rst = 1'd0;
+    wr_en = 1'd0;
+    rd_en = 1'd0;
+    data_in = 8'd0;
+    $monitor(" wr_en = %0d rd_en = %0d data in = %0d data out = %0d empty = %0d full = %0d", wr_en , rd_en, data_in, data_out, empty, full);
+    #10;
+    rst = 1'd1;
+    wr_en = 1'd1;
+    repeat(1025) begin;
+      data_in = data_in + 10;
+      #10;
     end
-    
-
-
-    initial begin
-        clk   = 0;
-        rst_n = 0;
-        wr_en = 0;
-        rd_en = 0;
-        din   = 0;
-
-#20;
-        rst_n = 1;
-
-
-        repeat (10) begin
-            @(posedge clk);
-            wr_en = 1;
-            din = din + 8'd1;
-        end
-        @(posedge clk);
-        wr_en = 0;
-
-
-        repeat (5) begin
-            @(posedge clk);
-            rd_en = 1;
-        end
-        @(posedge clk);
-        rd_en = 0;
-
-
-        while (!full) begin
-            @(posedge clk);
-            wr_en = 1;
-            din = din + 8'd1;
-        end
-        @(posedge clk);
-        wr_en = 0;
-        
-        while (!empty) begin
-            @(posedge clk);
-            rd_en = 1;
-        end
-        @(posedge clk);
-        rd_en = 0;
-        #20;
-        $finish;
-    end
-endmodule
-
-
-
-
-
-
+    #15;
+    $finish;
+  end
+endmodule 
+      
